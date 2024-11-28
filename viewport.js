cc7c7c460c66fd822e775462ea8957b3c673bf54
Iván Álvarez Fernández <ivan.alvarez.fernandez@udc.es>
2024-11-28 19:28:02 +0100

    [STYLES] Cambios en css y javascript para animaciones de viewport

---
+++ viewport.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            } else {
+                entry.target.classList.remove('visible');
+            }
+        });
+    }, { threshold: 0.5 });
+
+    document.querySelectorAll('.shadow-container').forEach(container => {
+        observer.observe(container);
+    });
+});
